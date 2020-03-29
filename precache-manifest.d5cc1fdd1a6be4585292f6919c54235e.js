d05a955591c432cb285abc662558314f67116d2a
Tanmoy <tanmoy@THPC0203.local>
2020-03-29 20:12:13 +0700

    Updates

---
+++ precache-manifest.d5cc1fdd1a6be4585292f6919c54235e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e077f8df1fc8f2ed374b7d7f922fd144",
+    "url": "/covid-aggregator/index.html"
+  },
+  {
+    "revision": "bbb01eba222d38b8bc2c",
+    "url": "/covid-aggregator/static/css/main.a88d4a93.chunk.css"
+  },
+  {
+    "revision": "f3364ea10ea687838c73",
+    "url": "/covid-aggregator/static/js/2.34033712.chunk.js"
+  },
+  {
+    "revision": "ccf491244da6a3591a7f18b2d9dc8874",
+    "url": "/covid-aggregator/static/js/2.34033712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb01eba222d38b8bc2c",
+    "url": "/covid-aggregator/static/js/main.31ad220d.chunk.js"
+  },
+  {
+    "revision": "dad332a54275bccbbd78",
+    "url": "/covid-aggregator/static/js/runtime-main.b0d93ace.js"
+  }
+]);
